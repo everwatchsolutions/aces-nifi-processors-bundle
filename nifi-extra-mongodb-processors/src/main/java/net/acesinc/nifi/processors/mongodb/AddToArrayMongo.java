3dad227ae9fe5d03e067ad2a5ec52650a4ac63a9
Andrew Serff <andrew.serff@acesinc.net>
2016-05-18 13:00:39 -0600

    Initial version

---
+++ nifi-extra-mongodb-processors/src/main/java/net/acesinc/nifi/processors/mongodb/AddToArrayMongo.java
@@ -0,0 +1,221 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.acesinc.nifi.processors.mongodb;
+
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoCollection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.nifi.annotation.behavior.EventDriven;
+import org.apache.nifi.annotation.behavior.InputRequirement;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ProcessorLog;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processors.mongodb.AbstractMongoProcessor;
+import org.apache.nifi.stream.io.StreamUtils;
+import org.bson.Document;
+
+/**
+ *
+ * @author andrewserff
+ */
+@EventDriven
+@Tags({ "mongodb", "insert", "update", "write", "put" })
+@InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
+@CapabilityDescription("Adds the contents of a FlowFile to a specified array in a MongoDB document")
+public class AddToArrayMongo extends AbstractMongoProcessor {
+    static final Relationship REL_SUCCESS = new Relationship.Builder().name("success")
+            .description("All FlowFiles that are written to MongoDB are routed to this relationship").build();
+    static final Relationship REL_FAILURE = new Relationship.Builder().name("failure")
+            .description("All FlowFiles that cannot be written to MongoDB are routed to this relationship").build();
+    
+    static final String MODE_SINGLE = "single";
+    static final String MODE_MANY = "many";
+    
+    static final String WRITE_CONCERN_ACKNOWLEDGED = "ACKNOWLEDGED";
+    static final String WRITE_CONCERN_UNACKNOWLEDGED = "UNACKNOWLEDGED";
+    static final String WRITE_CONCERN_FSYNCED = "FSYNCED";
+    static final String WRITE_CONCERN_JOURNALED = "JOURNALED";
+    static final String WRITE_CONCERN_REPLICA_ACKNOWLEDGED = "REPLICA_ACKNOWLEDGED";
+    static final String WRITE_CONCERN_MAJORITY = "MAJORITY";
+
+    static final PropertyDescriptor MODE = new PropertyDescriptor.Builder()
+        .name("Mode")
+        .description("Directs how we should handle the update query. If mode is single, then only a single record that matches the update query would be updated. If mode is many, all records that match would be updated.")
+        .required(true)
+        .allowableValues(MODE_SINGLE, MODE_MANY)
+        .defaultValue(MODE_SINGLE)
+        .build();
+    static final PropertyDescriptor UPDATE_QUERY_KEY = new PropertyDescriptor.Builder()
+        .name("Update Query Key")
+        .description("Key name used to build the update query criteria; this property must be present in the FlowFile content")
+        .required(true)
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .defaultValue("_id")
+        .build();
+    static final PropertyDescriptor ARRAY_NAME = new PropertyDescriptor.Builder()
+        .name("Name of Array to add to")
+        .description("Key name used to build the update query criteria; this property is valid only when using update mode, "
+                + "otherwise it is ignored")
+        .required(true)
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .build();
+    static final PropertyDescriptor WRITE_CONCERN = new PropertyDescriptor.Builder()
+        .name("Write Concern")
+        .description("The write concern to use")
+        .required(true)
+        .allowableValues(WRITE_CONCERN_ACKNOWLEDGED, WRITE_CONCERN_UNACKNOWLEDGED, WRITE_CONCERN_FSYNCED, WRITE_CONCERN_JOURNALED,
+            WRITE_CONCERN_REPLICA_ACKNOWLEDGED, WRITE_CONCERN_MAJORITY)
+        .defaultValue(WRITE_CONCERN_ACKNOWLEDGED)
+        .build();
+    static final PropertyDescriptor CHARACTER_SET = new PropertyDescriptor.Builder()
+        .name("Character Set")
+        .description("The Character Set in which the data is encoded")
+        .required(true)
+        .addValidator(StandardValidators.CHARACTER_SET_VALIDATOR)
+        .defaultValue("UTF-8")
+        .build();
+
+    private final static Set<Relationship> relationships;
+    private final static List<PropertyDescriptor> propertyDescriptors;
+
+    static {
+        List<PropertyDescriptor> _propertyDescriptors = new ArrayList<>();
+        _propertyDescriptors.add(URI);
+        _propertyDescriptors.add(DATABASE_NAME);
+        _propertyDescriptors.add(COLLECTION_NAME);
+        _propertyDescriptors.add(MODE);
+        _propertyDescriptors.add(UPDATE_QUERY_KEY);
+        _propertyDescriptors.add(ARRAY_NAME);
+        _propertyDescriptors.add(WRITE_CONCERN);
+        _propertyDescriptors.add(CHARACTER_SET);
+        propertyDescriptors = Collections.unmodifiableList(_propertyDescriptors);
+
+        final Set<Relationship> _relationships = new HashSet<>();
+        _relationships.add(REL_SUCCESS);
+        _relationships.add(REL_FAILURE);
+        relationships = Collections.unmodifiableSet(_relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return propertyDescriptors;
+    }
+    
+    @Override
+    public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+        final FlowFile flowFile = session.get();
+        if (flowFile == null) {
+            return;
+        }
+
+        final ProcessorLog logger = getLogger();
+
+        final Charset charset = Charset.forName(context.getProperty(CHARACTER_SET).getValue());
+        final WriteConcern writeConcern = getWriteConcern(context);
+
+        final String mode = context.getProperty(MODE).getValue();
+        final MongoCollection<Document> collection = getCollection(context).withWriteConcern(writeConcern);
+
+        try {
+            // Read the contents of the FlowFile into a byte array
+            final byte[] content = new byte[(int) flowFile.getSize()];
+            session.read(flowFile, new InputStreamCallback() {
+                @Override
+                public void process(final InputStream in) throws IOException {
+                    StreamUtils.fillBuffer(in, content, true);
+                }
+            });
+
+            // parse
+            // document should look like:
+//            {
+//              "_id": "myid",
+//              "myarray": "newValue"
+//            }
+            // or to add multiple values (I think this will work...
+//            {
+//              "_id": "myid",
+//              "myarray": { $each: ["newValue1", "newValue2"]}
+//            }
+            final Document doc = Document.parse(new String(content, charset));
+
+            // now set up the update
+            final String updateKey = context.getProperty(UPDATE_QUERY_KEY).getValue();
+            final Document query = new Document(updateKey, doc.get(updateKey));
+
+            final String arrayName = context.getProperty(ARRAY_NAME).getValue();
+            final Document arrayToAdd = new Document(arrayName, doc.get(arrayName));
+            final Document addToArrayOp = new Document("$addToSet", arrayToAdd);
+            
+            switch(mode) {
+                case MODE_SINGLE:
+                    collection.updateOne(query, addToArrayOp);
+                    break;
+                case MODE_MANY:
+                    collection.updateMany(query, addToArrayOp);
+                    break;
+            }
+            
+            logger.info("updated {} into MongoDB", new Object[] { flowFile });
+
+            session.getProvenanceReporter().send(flowFile, context.getProperty(URI).getValue());
+            session.transfer(flowFile, REL_SUCCESS);
+        } catch (Exception e) {
+            logger.error("Failed to insert {} into MongoDB due to {}", new Object[] {flowFile, e}, e);
+            session.transfer(flowFile, REL_FAILURE);
+            context.yield();
+        }
+    }
+    
+    protected WriteConcern getWriteConcern(final ProcessContext context) {
+        final String writeConcernProperty = context.getProperty(WRITE_CONCERN).getValue();
+        WriteConcern writeConcern = null;
+        switch (writeConcernProperty) {
+        case WRITE_CONCERN_ACKNOWLEDGED:
+            writeConcern = WriteConcern.ACKNOWLEDGED;
+            break;
+        case WRITE_CONCERN_UNACKNOWLEDGED:
+            writeConcern = WriteConcern.UNACKNOWLEDGED;
+            break;
+        case WRITE_CONCERN_FSYNCED:
+            writeConcern = WriteConcern.FSYNCED;
+            break;
+        case WRITE_CONCERN_JOURNALED:
+            writeConcern = WriteConcern.JOURNALED;
+            break;
+        case WRITE_CONCERN_REPLICA_ACKNOWLEDGED:
+            writeConcern = WriteConcern.REPLICA_ACKNOWLEDGED;
+            break;
+        case WRITE_CONCERN_MAJORITY:
+            writeConcern = WriteConcern.MAJORITY;
+            break;
+        default:
+            writeConcern = WriteConcern.ACKNOWLEDGED;
+        }
+        return writeConcern;
+    }
+    
+}
