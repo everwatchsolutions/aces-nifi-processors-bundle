49784872f1a9dd6637cb5d46b0c483278172f17e
Jeremy Taylor <jeremy.taylor@acesinc.net>
2016-08-26 19:06:33 -0400

    adding the ability to take in lists of different flow parameters for each different type you'd like them to be in JSON:  string list, a date list (isoDates for mongo), an int list, and a double list;

--- aces-nifi-processors/src/main/java/net/acesinc/nifi/processors/security/ConvertSecurityMarkingAndAttrListIntoJson.java
+++ aces-nifi-processors/src/main/java/net/acesinc/nifi/processors/security/ConvertSecurityMarkingAndAttrListIntoJson.java
@@ -10,13 +10,16 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
@@ -51,25 +54,59 @@ import org.apache.nifi.processor.util.StandardValidators;
 @WritesAttribute(attribute = "See additional details", description = "This processor may write or remove zero or more attributes as described in additional details")
 public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor {
 
-    public static final String CONFIG_FILE_NAME = "application.yaml";
-    public static final String DEFAULT_CLASSIFICATION_ATTR = "classificationType";
-    public static final String DEFAULT_COMPARTMENTS_ATTR = "compartments";
-    public static final String DEFAULT_RELEASABILITIES_ATTR = "releasabilities";
-    public static final String DEFAULT_DISSEM_CONTROLS_ATTR = "disseminationControls";
-    public static final String JSON_ATTRIBUTE_NAME = "JSONAttributes";
     private static final String AT_LIST_SEPARATOR = ",";
     private static final String APPLICATION_JSON = "application/json";
+    private static final String MONGO_TIME_ZONE = "GMT-0";
+    private static final String MONGO_DATE_TEMPLATE = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
-    public static final PropertyDescriptor ATTRIBUTES_LIST = new PropertyDescriptor.Builder()
-            .name("Attributes List")
-            .description("Comma separated list of attributes to be included in the resulting JSON. If this value "
+    /**
+     * to provide a list of attributes to serialize out as strings in JSON
+     */
+    public static final PropertyDescriptor STRING_ATTRIBUTES_LIST = new PropertyDescriptor.Builder()
+            .name("String Attributes List")
+            .description("Comma separated list of attributes to be included in the resulting JSON as String values. If this value "
                     + "is left empty then all existing Attributes will be included. This list of attributes is "
                     + "case sensitive. If an attribute specified in the list is not found it will be be emitted "
                     + "to the resulting JSON with an empty string or NULL value.")
             .required(false)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
+    /**
+     * to provide a list of attributes to serialize out as integers in JSON
+     */
+    public static final PropertyDescriptor INT_ATTRIBUTES_LIST = new PropertyDescriptor.Builder()
+            .name("Integer Attributes List")
+            .description("Comma separated list of attributes to be included in the resulting JSON as Integer values. If this value "
+                    + "is left empty then this is ignored.")
+            .required(false)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    /**
+     * to provide a list of attributes to serialize out as doubles in JSON
+     */
+    public static final PropertyDescriptor DOUBLE_ATTRIBUTES_LIST = new PropertyDescriptor.Builder()
+            .name("Double Attributes List")
+            .description("Comma separated list of attributes to be included in the resulting JSON as Double values. If this value "
+                    + "is left empty then this is ignored.")
+            .required(false)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    /**
+     * to provide a list of attributes to serialize out as ISODates for mongo in
+     * JSON
+     */
+    public static final PropertyDescriptor EPOCH_TO_DATES_ATTRIBUTES_LIST = new PropertyDescriptor.Builder()
+            .name("Epoch Dates Attributes List (Longs)")
+            .description("Comma separated list of attributes to be included in the resulting JSON that will be converted from Long Epoch values to Date values. If this value "
+                    + "is left empty then this is ignored.")
+            .required(false)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
 
+    /**
+     * a config in json that will be deserialized as a FlowAttrSecurityConfig
+     * object. See that other class for details.
+     */
     public static final PropertyDescriptor CONVERTER_CONFIG = new PropertyDescriptor.Builder()
             .name("converter-config")
             .displayName("Converter Config")
@@ -79,6 +116,10 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
             .expressionLanguageSupported(false)
             .build();
 
+    /**
+     * This is where you pass in the flow attribute expression that holds the
+     * raw security marking.
+     */
     public static final PropertyDescriptor RAW_SECURITY_MARKING_FROM_FILE_NAME = new PropertyDescriptor.Builder()
             .name("Raw security marking from file name")
             .description("The raw security marking attribute will NOT be included in the resulting String. If this value "
@@ -99,7 +140,10 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
     static {
 
         final List<PropertyDescriptor> _properties = new ArrayList<>();
-        _properties.add(ATTRIBUTES_LIST);
+        _properties.add(STRING_ATTRIBUTES_LIST);
+        _properties.add(INT_ATTRIBUTES_LIST);
+        _properties.add(DOUBLE_ATTRIBUTES_LIST);
+        _properties.add(EPOCH_TO_DATES_ATTRIBUTES_LIST);
         _properties.add(CONVERTER_CONFIG);
         _properties.add(RAW_SECURITY_MARKING_FROM_FILE_NAME);
         properties = Collections.unmodifiableList(_properties);
@@ -163,6 +207,7 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
             regexPrefixForClass.append(classPart);
             regexPrefixForClass.append("|");
         }
+        //this is awkward, but does the trick
         int lastIndexOfPipe = regexPrefixForClass.lastIndexOf("|");
         regexPrefixForClass = regexPrefixForClass.replace(lastIndexOfPipe, regexPrefixForClass.length(), "");
         regexPrefixForClass.append(")");
@@ -228,7 +273,10 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
      *
      * @param ff
      * @param rawSecurityMarking
-     * @param atrList
+     * @param atrListForStringValues
+     * @param atrListForIntValues
+     * @param atrListForDoubleValues
+     * @param atrListForLongEpochToGoToDateValues
      * @param secConfig
      * @return Map of values that are feed to a Jackson ObjectMapper
      * @throws java.io.IOException
@@ -236,14 +284,18 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
     protected Map<String, Object> buildSecurityAttributesMapForFlowFileAndBringInFlowAttrs(
             FlowFile ff,
             String rawSecurityMarking,
-            String atrList,
+            String atrListForStringValues,
+            String atrListForIntValues,
+            String atrListForDoubleValues,
+            String atrListForLongEpochToGoToDateValues,
             FlowAttrSecurityConfig secConfig) throws IOException {
         final ComponentLog logger = getLogger();
         Map<String, Object> atsToWrite = new HashMap<>();
 
+        //handle all the string values
         //If list of attributes specified get only those attributes. Otherwise write them all
-        if (StringUtils.isNotBlank(atrList)) {
-            String[] ats = StringUtils.split(atrList, AT_LIST_SEPARATOR);
+        if (StringUtils.isNotBlank(atrListForStringValues)) {
+            String[] ats = StringUtils.split(atrListForStringValues, AT_LIST_SEPARATOR);
             if (ats != null) {
                 for (String str : ats) {
                     String cleanStr = str.trim();
@@ -259,8 +311,62 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
         } else {
             atsToWrite.putAll(ff.getAttributes());
         }
+        //handle all int values
+        if (StringUtils.isNotBlank(atrListForIntValues)) {
+            String[] ats = StringUtils.split(atrListForIntValues, AT_LIST_SEPARATOR);
+            if (ats != null) {
+                for (String str : ats) {
+                    String cleanStr = str.trim();
+                    String val = ff.getAttribute(cleanStr);
+                    if (val != null) {
+                        atsToWrite.put(cleanStr, Integer.parseInt(val));
+                    } else {
+                        atsToWrite.put(cleanStr, null);
+                    }
+                }
+            }
+        }
+        //handle all double values
+        if (StringUtils.isNotBlank(atrListForDoubleValues)) {
+            String[] ats = StringUtils.split(atrListForDoubleValues, AT_LIST_SEPARATOR);
+            if (ats != null) {
+                for (String str : ats) {
+                    String cleanStr = str.trim();
+                    String val = ff.getAttribute(cleanStr);
+                    if (val != null) {
+                        atsToWrite.put(cleanStr, Double.parseDouble(val));
+                    } else {
+                        atsToWrite.put(cleanStr, null);
+                    }
+                }
+            }
+        }
+        //handle all date values
+        if (StringUtils.isNotBlank(atrListForLongEpochToGoToDateValues)) {
+            String[] ats = StringUtils.split(atrListForLongEpochToGoToDateValues, AT_LIST_SEPARATOR);
+            if (ats != null) {
+                for (String str : ats) {
+                    String cleanStr = str.trim();
+                    String val = ff.getAttribute(cleanStr);
+                    if (val != null) {
+                        long epochTime = Long.parseLong(val);
+                        GregorianCalendar gcal = new GregorianCalendar();
+                        gcal.setTimeZone(TimeZone.getTimeZone(MONGO_TIME_ZONE));
+                        gcal.setTimeInMillis(epochTime);
+                        SimpleDateFormat sdf = new SimpleDateFormat(MONGO_DATE_TEMPLATE);
+                        String mongoDate = sdf.format(gcal.getTime());
+                        //to Date
+                        Map<String, String> isoDate = new HashMap<>();
+                        isoDate.put("$date", mongoDate);
+                        atsToWrite.put(cleanStr, isoDate);
+                    } else {
+                        atsToWrite.put(cleanStr, null);
+                    }
+                }
+            }
+        }
 
-        //If list of attributes specified get only those attributes. Otherwise write them all
+        //build the classification object for json
         if (StringUtils.isNotBlank(rawSecurityMarking)) {
             Classification classification = new Classification();
             String cleanRawMarking = rawSecurityMarking.trim();
@@ -294,7 +400,10 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
             FlowAttrSecurityConfig secConfig = objectMapper.readValue(context.getProperty(CONVERTER_CONFIG).getValue(), FlowAttrSecurityConfig.class);
             final Map<String, Object> attrsToUpdate = buildSecurityAttributesMapForFlowFileAndBringInFlowAttrs(original,
                     context.getProperty(RAW_SECURITY_MARKING_FROM_FILE_NAME).evaluateAttributeExpressions(original).getValue(),
-                    context.getProperty(ATTRIBUTES_LIST).getValue(),
+                    context.getProperty(STRING_ATTRIBUTES_LIST).getValue(),
+                    context.getProperty(INT_ATTRIBUTES_LIST).getValue(),
+                    context.getProperty(DOUBLE_ATTRIBUTES_LIST).getValue(),
+                    context.getProperty(EPOCH_TO_DATES_ATTRIBUTES_LIST).getValue(),
                     secConfig);
 
             FlowFile conFlowfile = session.write(original, new StreamCallback() {
@@ -310,7 +419,7 @@ public class ConvertSecurityMarkingAndAttrListIntoJson extends AbstractProcessor
             session.transfer(conFlowfile, REL_SUCCESS);
 
         } catch (IOException ioe) {
-            getLogger().error(ioe.getMessage());
+            logger.error(ioe.getMessage());
             session.transfer(original, REL_FAILURE);
         }
     }
